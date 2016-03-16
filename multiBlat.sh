2e14cb7f89f2c3ca28ddfceb7d36cc45ae4ff3c3
jsteenbrugge <you@example.com>
2016-03-16 15:50:59 +0100

    added new scripts

--- multiBlat.sh
+++ multiBlat.sh
@@ -1,5 +1,5 @@
 for i in ${1}*
 do
 	echo $i
-	blat -t=dnax -q=prot blat/xt9_0.2bit $i blat/$(basename $i .fa).psl &
+	blat -t=dnax -q=prot ~/data/aQueenslandica/genome/Amphimedon_queenslandica.Aqu1.30.dna_sm.genome.2bit $i blat/$(basename $i .fa).psl &
 done
