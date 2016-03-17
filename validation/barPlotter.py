a8686874299b549311c2f71f6931805a288f2677
Joris van Steenbrugge <jorisvansteenbrugge@hotmail.com>
2016-03-17 14:41:31 +0100

    barplotter fix

--- validation/barPlotter.py
+++ validation/barPlotter.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn
+import os
 
 
-def plot(names, values):
+def plot(names, values, dir):
+	os.chdir(dir)
 	y_pos = np.arange(len(names))
 	plt.bar(y_pos, values, align='center', alpha = 0.8)
 	plt.xticks(y_pos, names)
 	plt.ylabel('H3K4me3 genes')
 	plt.title('H3K4me3 genes per bed file')
-	plt.savefig('barplot.eps', format='eps', dpi=1000)
+	plt.savefig('barplot.svg', format='svg', dpi=1200)
