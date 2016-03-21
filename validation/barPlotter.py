bfbd581fe2e34a7272a9a3936e944f02902e576d
jsteenbrugge <you@example.com>
2016-03-21 09:34:32 +0100

    better axis title

--- validation/barPlotter.py
+++ validation/barPlotter.py
@@ -10,6 +10,6 @@ def plot(names, values, dir):
 	y_pos = np.arange(len(names))
 	plt.bar(y_pos, values, align='center', alpha = 0.8)
 	plt.xticks(y_pos, names)
-	plt.ylabel('H3K4me3 genes')
+	plt.ylabel('No. of H3K4me3 genes')
 	plt.title('H3K4me3 genes per bed file')
 	plt.savefig('barplot.svg', format='svg', dpi=1200)
