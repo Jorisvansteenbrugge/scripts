8d506f4b62a972ee7416303247ad6c15605e0c0b
jsteenbrugge <you@example.com>
2016-03-21 11:59:20 +0100

    axis

--- validation/boxPlotter.R
+++ validation/boxPlotter.R
@@ -4,6 +4,6 @@ library(ggplot2)
 args <- commandArgs(trailingOnly = TRUE)
 rawdata = read.table(args[1], header = TRUE, fill=TRUE)
 data = melt(rawdata)
-ggplot(data, aes(x= variable, y=value))+ geom_boxplot()+ xlab("Annotation files") + ylab("RPKM for each gene") +scale_y_continuous(breaks = round(seq(0, 40, by= 2),1))
+ggplot(data, aes(x= variable, y=value))+ geom_boxplot()+ xlab("Annotation files") + ylab("2LOG RPKM for each gene") +scale_y_continuous(breaks = round(seq(0, 40, by= 2),1))
 
 ggsave(filename = args[2], dpi = 1200)
