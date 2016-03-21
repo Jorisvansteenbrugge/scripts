b96a1a2d62998cfb23992c7fdebdd06489099165
jsteenbrugge <you@example.com>
2016-03-21 09:35:52 +0100

    better axis title

--- validation/boxPlotter.R
+++ validation/boxPlotter.R
@@ -4,6 +4,6 @@ library(ggplot2)
 args <- commandArgs(trailingOnly = TRUE)
 rawdata = read.table(args[1], header = TRUE, fill=TRUE)
 data = melt(rawdata)
-plot = ggplot(data, aes(x= variable, y=value))+ geom_boxplot()+scale_y_log10()
+plot = ggplot(data, aes(x= bed files, y=RPKM per gene))+ geom_boxplot()+scale_y_log10()
 
 ggsave(filename = args[2], dpi = 1200, scale = 2)
