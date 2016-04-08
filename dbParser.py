80c387a3b40ccda9348c64a9521055f098476d8b
Joris van Steenbrugge <jorisvansteenbrugge@hotmail.com>
2016-04-08 16:02:03 +0200

    idk

--- dbParser.py
+++ dbParser.py
@@ -4,7 +4,6 @@ import sys
 
 dbFile = sys.argv[1]
 range = 1000
-
 class DB:
     cursor = None
     
@@ -17,19 +16,21 @@ class DB:
             print("Cannot connect to {0}".format(dbFile))
             sys.exit(1)
 
-    def search(self, line):
+    def search(self, chrom,start,end,strand):
         if line != "":
             chrom = line[0]
             start = line[1]
             end = line[2]
             strand = line[5]
 
-            statement = "SELECT id FROM feature WHERE chrom='{0}' AND start BETWEEN'{1}' AND '{2}' AND end BETWEEN '{3}' AND '{4}' AND strand='{5}'".format(chrom,int(start)-range,int(start)+range,int(end)-range, int(end)+range, strand)
-            self.cursor.execute(statement)
+            betweenstatement = "SELECT id FROM feature WHERE chrom='{0}' AND start BETWEEN'{1}' AND '{2}' AND end BETWEEN '{3}' AND '{4}' AND strand='{5}'".format(chrom,int(start)-range,int(start)+range,int(end)-range, int(end)+range, strand)
+
+            self.cursor.execute(betweenstatement)
 
             rows = self.cursor.fetchall()
             id = rows[0][0]
-            return self.getName(id)
+            return id
+            #return self.getName(id)
             
     def getName(self,id):
         substatement = "SELECT evidence_id FROM feature_evidence WHERE feature_id='{0}'".format(id)
@@ -37,6 +38,7 @@ class DB:
         self.cursor.execute(statement)
         rows = self.cursor.fetchone()
         return rows[0].split(":::")[1]
+    
 
 bed = "/home/joris/Desktop/test.bed"
 database = DB()
@@ -45,10 +47,22 @@ with open(bed) as f:
     for i in f:
         line = i.strip().split("\t")
         try:
-            line[3] = database.search(line)
+            ids = []
+            start = int(line[1])
+            blSizes = line[10].split(",")
+            blStarts = line[11].split(",")
+            for x,y in enumerate(blSizes):
+                try:
+                    size = int(y)
+                    chr  = line[0]
+                    st   = start+int(blStarts[x])
+                    end  = st+size
+                    ids.append(database.search(chr,st,end,line[5]))
+                except:
+                    pass
+            ids = list(set(ids))
+            if len(ids) == 1:
+                line[3] = database.getName(ids[0])
         except IndexError:
             pass
         print("\t".join(line))
-         
-
-        
