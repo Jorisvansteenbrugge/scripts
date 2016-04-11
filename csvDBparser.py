6fd4b64dffa5e4a8caea123f1798e0cc95040ee0
Joris van Steenbrugge <jorisvansteenbrugge@hotmail.com>
2016-04-11 10:27:49 +0200

    wip

--- csvDBparser.py
+++ csvDBparser.py
@@ -1,15 +1,19 @@
-from TemporaryFile import NamedTemporaryFile as namedTmp
+from tempfile import NamedTemporaryFile as namedTmp
 import sys
 import subprocess as sp
 
 dbFile = sys.argv[1]
+bedFile = sys.argv[2]
 tables = ["feature","evidence","feature_evidence"]
 
-[createDBFiles(x) for x in tables]
+
+
 
 
 
 
 def createDBFiles(table):
-    cmd = "sqlite3 -header -csv {0} \" SELECT * FROM {1};\" > /tmp/{1}.csv".format(dbFile,table)
+    cmd = "sqlite3 -csv {0} \" SELECT * FROM {1};\" > /tmp/{1}.csv".format(dbFile,table)
     sp.call(cmd, shell=True)
+
+[createDBFiles(x) for x in tables]
