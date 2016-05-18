9fa5219b3aae1664894a8ed06de1e80e5d817863
jsteenbrugge <you@example.com>
2016-05-18 22:21:58 +0200

    wip

--- validation/peakGBar.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib
-import seaborn as sns
-
-df = pd.read_csv("/home/joris/degroteswagfile", sep ="\t")
-sns.set_style("whitegrid")
-sns.barplot(x="bed",y="h3",data=df, hue="peak")
-matplotlib.pyplot.savefig("/home/joris/barPlotNEW.svg")
