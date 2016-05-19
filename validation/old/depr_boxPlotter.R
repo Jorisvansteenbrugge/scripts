6f0c4b235e34fd5142c9da75d562b439f98a66e2
jsteenbrugge <you@example.com>
2016-05-19 10:50:18 +0200

    removed old files

--- validation/old/depr_boxPlotter.R
+++
@@ -1,9 +0,0 @@
-library(reshape2)
-library(ggplot2)
-
-args <- commandArgs(trailingOnly = TRUE)
-rawdata = read.table(args[1], header = TRUE, fill=TRUE)
-data = melt(rawdata)
-ggplot(data, aes(x= variable, y=value))+ geom_boxplot()+ xlab("Annotation files") + ylab("2LOG RPKM for each gene") +scale_y_continuous(breaks = round(seq(0, 40, by= 2),1))
-
-ggsave(filename = args[2], dpi = 1200)
