5ab616aab1d920256f0cc615e29c12d9b7b50f40
Joris van Steenbrugge <jorisvansteenbrugge@hotmail.com>
2016-05-19 09:54:08 +0200

    moving around

--- signiCheck.R
+++ signiCheck.R
@@ -1,5 +1,7 @@
 args <- commandArgs(trailingOnly = TRUE)
-file = read.table(args[1],sep = "\t",header=TRUE)
+
+
+file = read.table("/run/user/1000/gvfs/sftp:host=mb07.science.ru.nl/home/jsteenbrugge/calculations/xtFullRun/aed/pitavsxenbaseMRNA.tsv",sep = "\t",header=TRUE)
 col1 <- as.vector(file[,1])
 col2 <- as.vector(file[,2])
 result = wilcox.test(col1,col2, alternative = "two.sided")
