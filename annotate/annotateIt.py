11570b0f8e6901f52c25a6869576d4ba34e9222c
jsteenbrugge <you@example.com>
2016-05-23 16:43:25 +0200

    wip

--- annotate/annotateIt.py
+++ annotate/annotateIt.py
@@ -4,38 +4,94 @@ from Bio import SeqIO
 from sys import argv
 from tempfile import NamedTemporaryFile as namedTemp
 import subprocess as sp
+import argparse
 
-def parseFasta():
-#    outfile = open(argv[2],"w")
-    fasta_seqs = SeqIO.parse(open(argv[1]),"fasta")
+
+
+
+def parseFasta(fastaF,outF, bed):
+    outfile = open(outF,"w")
+    fasta_seqs = SeqIO.parse(open(fastaF),"fasta")
     count = 0
-
+    print("fasta parse")
     for fasta in fasta_seqs:
-        query = namedTemp(suffix=".fa")
+        query = namedTemp(delete=False)
+        print("fasta found")
         if count < 2:
             name, seq = fasta.id, str(fasta.seq)
-            query.write(">{}\n{}".format(name,seq))
+            seqout = ">{}\n{}\n".format(name,seq)
+            print(seqout)
+            print(query.name)
+            query.write(seqout)
             query.flush()
-            if annotateSeq(query.name):
-                continue
+            
+            #calling the blastp program. Resulting in the file with the blast results
+            resultFile = annotateSeq(query.name)
+            
+            print("result file")
+            pos, refID = getBestResult(resultFile)
+            if refID != "":
+                outfile.write("\t".join(improveBed(pos,refID, bed))+"\n")
+                print("Something happened")
             else:
-                break
+                print("Passed")
         else:
             return True
         count+=1
         query.close()
 
 def annotateSeq(queryFile):
-    resultFile = namedTemp()
-    CMD = "blastn -db nt -query {} -out {} -remote".format(queryFile, resultFile.name)
+    resultFile = namedTemp(delete=False)
+    CMD = "blastp -db nr -query {} -out {} -remote".format(queryFile, resultFile.name)
     sp.call(CMD,shell=True)
-    return 0
-    
-    
+    print(resultFile.name)
+    return resultFile.name
 
+def improveBed(pos, refID,bed):
+    print("improve")
+    bedPos = pos.replace(":","\t")
+    bedPos = bedPos.replace("-","\t")
+    bedPos = bedPos.replace("_","")
+
+    interFile = namedTemp()
+    interFile.write(bedPos+"\n")
+    interFile.flush()
+
+    CMD = "bedtools intersect -a {} -b {}".format(bed,interFile.name)
+    handle = sp.Popen(CMD,shell=True,stdout=sp.PIPE)
+    line = handle.stdout.readline().strip()
+    line = line.split("\t")
+
+        
+    line[3] = refID
+    return line
+
+def getBestResult(resultFile):
+    print("best result")
+    pos = ""
+    refID = ""
+    with open(resultFile) as inf:
+        for line in inf:
+            if "Query=" in line and pos=="":
+                pos = line.split("= ")[1].strip()
+            elif "ref|" in line and "XP_" not in line:
+                refID = line.split("|")[1]
+            elif "gb|" in line:
+                refID = line.split("|")[1]
+            if pos != "" and refID != "":
+                return pos, refID
+
+    
+def parseArgs():
+    p = argparse.ArgumentParser()
+    p.add_argument("-p", dest="prot", help="Fasta file with protein sequences", required=True)
+    p.add_argument("-b", dest="bed", help="Bed file with existing genome annotations", required=True)
+    p.add_argument("-o", dest="output", help="Output bed file", required=True)
+    return p.parse_args()
 
 
 if __name__ == "__main__":
-    if parseFasta():
+    args = parseArgs()
+    if parseFasta(args.prot, args.output, args.bed):
         print("1000 records parsed")
     
