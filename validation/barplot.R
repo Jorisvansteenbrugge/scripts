49432c53a2df9be22b68637e1b796fdfdd13da23
Joris van Steenbrugge <jorisvansteenbrugge@hotmail.com>
2016-05-27 16:11:37 +0200

    testing

--- validation/barplot.R
+++ validation/barplot.R
@@ -5,7 +5,7 @@ library(data.table)
 
 cbbPalette <- c("steelblue", "steelblue3", "steelblue2", "steelblue1","steelblue","steelblue3","steelblue2")
 scaling <- c(10,20,30,40,50,60,70,80,90,100)
-df= read.table("/home/joris/data/newBoxplotdata.tsv", header = TRUE, fill = TRUE)
+df= read.table("/home/joris/peakGene.tsv", header = TRUE, fill = TRUE)
 x <- ""
 y <- "No. of genes w/ peaks"
 order = c("st6","st8", "st9","st9_2","st10.5","st11_ni","st11","st11a","st12","st16","st30")
@@ -19,13 +19,15 @@ as.data.frame(df) -> df
 
 ggplot(data=df, aes(annotation, percentage, fill=annotation)) +
   geom_bar(stat="identity")+
-  ggtitle("Stage")+
+  ggtitle("percentage of peaks intersecting with a gene start (relative to the amount of genes)")+
   scale_y_continuous(limits = c(0,100), expand = c(0,0),breaks = scaling)+
   guides(fill=FALSE)+
   labs(x=x, y=y) +
   theme(axis.text.x = element_text(angle = 50, hjust = 1))+
   facet_wrap(~st)
 
+ggplot(dafa=df, aes(annotation,percentage, fill=annotation))+
+  geom_bar(stat="identity")
 
 
 
