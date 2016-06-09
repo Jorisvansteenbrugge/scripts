e368d3baa10ee98ac73968655f27faaa4bd18643
Joris van Steenbrugge <jorisvansteenbrugge@hotmail.com>
2016-06-09 13:01:55 +0200

    autoscaling

--- validation/barplot.R
+++ validation/barplot.R
@@ -1,26 +1,26 @@
 #!/usr/bin/Rscript
-
 library(ggplot2)
 library(gridExtra)
 library(data.table)
 
-
-cbbPalette <- c("steelblue", "steelblue3", "steelblue2", "steelblue1","steelblue","steelblue3","steelblue2")
+#aesthetics
 scaling <- c(10,20,30,40,50,60,70,80,90,100)
+x <- "Annotations"
+y <- "% of peaks where a gene starts"
+
+
 
 args <- commandArgs(trailingOnly = TRUE)
-
-
-
 df= read.table(args[1], header = TRUE, fill = TRUE)
-x <- "Annotations"
-y <- "% of peaks with gene start"
+
 
 as.data.table(df) -> df
 df[,percentage:=count/total*100]
-as.data.frame(df) -> df
+df2 <- df[order(df$percentage),]
+df2$annotation <- as.character(df2$annotation)
+df2$annotation <- factor(df2$annotation, levels=unique(df2$annotation))
 
-ggplot(data=df, aes(annotation, percentage, fill=annotation)) +
+ggplot(data=df2, aes(df2$annotation, df2$percentage, fill=annotation)) +
   geom_bar(stat="identity")+
   theme_classic() +
   ggtitle("percentage of peaks matching with at least 1 gene start")+
@@ -28,7 +28,6 @@ ggplot(data=df, aes(annotation, percentage, fill=annotation)) +
   guides(fill=FALSE)+
   labs(x=x, y=y) +
   theme(axis.text.x = element_text(angle = 50, hjust = 1))
-#  facet_wrap(~st)
 
 ggsave(args[2])
 
